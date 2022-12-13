72ff89af1b3306135688109761d18d2f25c184b5
cleisommais <cleisommais@gmail.com>
2022-12-13 08:07:10 -0600

    Fixing the seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,29 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Coin.create!([
-               {
-                 description: 'Bitcoin',
-                 acronym: 'BTN',
-                 img_url: 'https://png.pngtree.com/element_our/20190601/ourmid/pngtree-yellow-bitcoin-free-map-image_1369249.jpg'
-               },
-               {
-                 description: 'Ethereum',
-                 acronym: 'ETH',
-                 img_url: 'https://banner2.cleanpng.com/20190418/rgf/kisspng-ethereum-portable-network-graphics-computer-icons-developers-icon-request-icon-ethereum-5cb941c1cb12b1.1213852915556448658318.jpg'
-               },
-               {
-                 description: 'XRP',
-                 acronym: 'XRP',
-                 img_url: 'https://w7.pngwing.com/pngs/35/349/png-transparent-xrp-cryptocurrency-logo-icon.png'
-               },
-               {
-                 description: 'Cardano',
-                 acronym: 'ADA',
-                 img_url: 'https://cdn4.iconfinder.com/data/icons/crypto-currency-and-coin-2/256/cardano_ada-512.png'
-               }
-             ])
-
 MiningType.create!([
                      {
                        description: 'Proof of Work',
@@ -37,5 +14,36 @@ MiningType.create!([
                      {
                        description: 'Proof of Stake',
                        acronym: 'PoS'
+                     },
+                     {
+                       description: 'Proof of Capacity',
+                       acronym: 'PoC'
                      }
                    ])
+
+Coin.create!([
+               {
+                 description: 'Bitcoin',
+                 acronym: 'BTN',
+                 img_url: 'https://png.pngtree.com/element_our/20190601/ourmid/pngtree-yellow-bitcoin-free-map-image_1369249.jpg',
+                 mining_type: MiningType.find_by(acronym: 'PoW')
+               },
+               {
+                 description: 'Ethereum',
+                 acronym: 'ETH',
+                 img_url: 'https://banner2.cleanpng.com/20190418/rgf/kisspng-ethereum-portable-network-graphics-computer-icons-developers-icon-request-icon-ethereum-5cb941c1cb12b1.1213852915556448658318.jpg',
+                 mining_type: MiningType.all.sample
+               },
+               {
+                 description: 'XRP',
+                 acronym: 'XRP',
+                 img_url: 'https://w7.pngwing.com/pngs/35/349/png-transparent-xrp-cryptocurrency-logo-icon.png',
+                 mining_type: MiningType.all.sample
+               },
+               {
+                 description: 'Cardano',
+                 acronym: 'ADA',
+                 img_url: 'https://cdn4.iconfinder.com/data/icons/crypto-currency-and-coin-2/256/cardano_ada-512.png',
+                 mining_type: MiningType.all.sample
+               }
+             ])
