fbcc40de446497e77cfa6434a20a9c223774ca12
cleisommais <cleisommais@gmail.com>
2022-12-14 07:31:16 -0600

    using cookie to setup the i18n

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,11 @@
 class ApplicationController < ActionController::Base
-    around_action :switch_locale
+    before_action :switch_locale
     # if found the query param 'locale', it will change the translation
     # http://localhost:3000?locale=en
-    def switch_locale(&action)
-      locale = params[:locale] || I18n.default_locale
-      I18n.with_locale(locale, &action)
+    def switch_locale
+      cookies[:locale] = params[:locale] if params[:locale]
+      return unless cookies[:locale]
+
+      I18n.locale = cookies[:locale] if I18n.locale != cookies[:locale]
     end
 end
