794b1474b399678008d115d2a11f260401feb284
cleisommais <cleisommais@gmail.com>
2022-12-14 07:23:41 -0600

    added one details/comment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
     around_action :switch_locale
     # if found the query param 'locale', it will change the translation
+    # http://localhost:3000?locale=en
     def switch_locale(&action)
       locale = params[:locale] || I18n.default_locale
       I18n.with_locale(locale, &action)
