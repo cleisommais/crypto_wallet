c920670041fcda169c940a93be06db4ddec05634
cleisommais <cleisommais@gmail.com>
2022-12-13 20:38:15 -0600

    Added i18n keys for same examples

--- config/application.rb
+++ config/application.rb
@@ -12,8 +12,8 @@ module CryptoWallet
     config.load_defaults 5.2
     #config.i18n.load_path += Dir[Rails.root.join('lib', 'locale', '*.{rb,yml}')]
     # Permitted locales available for the application
-    config.i18n.available_locales = %i[en 'pt-BR']
-    config.i18n.default_locale = 'pt-BR'
+    config.i18n.available_locales = %i[en pt]
+    config.i18n.default_locale = 'pt'
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
